c6f8a825dd79864e1b9b53e6a0aedc49029d7a68
Daan Lubbers <github.com.fn7i9@alias.daanlubbers.nl>
2025-02-19 22:47:49 +0100

    initial implementation with app factory

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,164 @@
 use std::{
     sync::{mpsc, Arc, Mutex},
-    thread
+    thread,
+    net::{TcpListener, TcpStream},  
+    io::{BufReader, BufRead, Write},
+    fs,
 };
+use core::fmt::{self, Display};
 
-pub struct ThreadPool {
+#[derive(PartialEq)]
+pub enum RequestType {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+}
+
+pub enum StatusCode {
+    OK,
+    NotFound,
+    InternalServerError,
+}
+
+impl Display for StatusCode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let output;
+        match *self {
+            StatusCode::OK => output = "HTTP/1.1 200 OK",
+            StatusCode::NotFound => output = "HTTP/1.1 404 NOT FOUND",
+            StatusCode::InternalServerError => output = "HTTP/1.1 500 INTERNAL SERVER ERROR",
+        }
+        write!(f, "{}", output)
+    }
+}
+
+pub struct Resource {
+    request_type: RequestType,
+    path: &'static str,
+    handler: fn() -> Result<Response, String>,
+}
+
+impl Resource {
+    pub fn new(request_type: RequestType, path: &'static str, handler: fn() -> Result<Response, String>) -> Resource {
+        Resource { request_type, path, handler }
+    }
+
+    pub fn handle(&self) -> Result<Response, String> {
+        (self.handler)()
+    }
+}
+
+pub struct Response {
+    status_code: StatusCode,
+    file: &'static str,
+}
+
+impl Response {
+    pub fn new(status_code: StatusCode, file: &'static str) -> Response {
+        Response { status_code, file }
+    }
+}
+
+pub struct AppConfig {
+    ip: &'static str,
+    port: u16,
+    num_threads: usize,
+}
+
+impl AppConfig {
+    pub fn new(ip: &'static str, port: u16, num_threads: usize) -> AppConfig {
+        AppConfig { ip, port, num_threads }
+    }
+}
+
+pub struct App {
+    config: AppConfig,
+    resources: Vec<Resource>,
+}
+
+impl App {
+    pub fn new(config: AppConfig) -> App {
+        App { config , resources: vec![]}
+    }
+
+    pub fn run(self) {
+        let ip = self.config.ip;
+        let port = self.config.port;
+        let listener = TcpListener::bind(format!("{ip}:{port}")).unwrap();
+        let pool = ThreadPool::new(self.config.num_threads);
+        let app = Arc::new(self);
+    
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+            let app_clone = Arc::clone(&app);
+    
+            pool.execute(move || { app_clone.handle_request(stream) });
+        }
+    }   
+
+    pub fn register_resource(&mut self, resource: Resource) {
+        self.resources.push(resource);
+    }
+
+    fn get_resource(&self, request_type: RequestType, path: &str) -> Option<&Resource> {
+        self.resources.iter().find(|resource| resource.request_type == request_type && resource.path == path)
+    }
+
+    fn handle_resource(&self, resource: &Resource, stream: &mut TcpStream) {
+        let response = resource.handle().unwrap();
+        let filename = response.file;
+        let status = response.status_code;
+    
+        let content = fs::read_to_string(filename).unwrap();
+        let length = content.len();
+        let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+        self.send_response(stream, response);
+    }
+
+    fn handle_not_found(&self, stream: &mut TcpStream) {
+        let response = format!("{}\r\n\r\n", StatusCode::NotFound);
+        self.send_response(stream, response);
+    }
+
+    fn send_response(&self, stream: &mut TcpStream, response: String) {
+        print!("Response: {response}");
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+ 
+    fn handle_request(&self, mut stream: TcpStream) {
+        let buf_reader = BufReader::new(&stream);
+        let request_line = buf_reader.lines().next().unwrap().unwrap();
+        
+        print!("Request: {request_line}\n");
+
+        let parts = request_line.split_whitespace().collect::<Vec<&str>>();
+
+        if parts.len() < 2 {
+            // Handle error case when request line is malformed
+            return;
+        }
+
+        let request_type = match parts[0] {
+            "GET" => RequestType::GET,
+            "POST" => RequestType::POST,
+            "PUT" => RequestType::PUT,
+            "DELETE" => RequestType::DELETE,
+            _ => RequestType::GET, // todo unsupported request
+        };
+
+        let path = parts[1];
+
+        let resource = self.get_resource(request_type, path);
+        match resource {
+            Some(resource) => self.handle_resource(resource, &mut stream),
+            None => self.handle_not_found(&mut stream),
+        }
+    }
+}
+
+struct ThreadPool {
     workers: Vec<Worker>,
     sender: Option<mpsc::Sender<Job>>,
 }
@@ -16,7 +171,7 @@ impl ThreadPool {
     /// # Panics
     ///
     /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
+    fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
@@ -35,7 +190,7 @@ impl ThreadPool {
         }
     }
 
-    pub fn execute<F>(&self, f: F)
+    fn execute<F>(&self, f: F)
     where F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},  
-    thread,
-    time::Duration,
-};
-
-use wwwdaanlubbersnl::ThreadPool;
-
-// Note: handle panics
+use wwwdaanlubbersnl::{App, AppConfig, RequestType, StatusCode, Resource, Response};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    let pool = ThreadPool::new(4);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(|| { handle_connection(stream) });
-    }
-
-    println!("Shutting down.");
+    let config = AppConfig::new("127.0.0.1", 7878, 4);
+    let app = create_app(config);
+    app.run();
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+fn create_app(config: AppConfig) -> App {
+    let mut app = App::new(config);
 
-    let (status, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "html/index.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "html/index.html")
-        },
-        _ => ("HTTP/1.1 404 NOT FOUND", "html/404.html"),
-    };
+    app.register_resource(Resource::new(RequestType::GET, "/", || {
+        Ok(Response::new(StatusCode::OK, "html/index.html"))      
+    }));
 
-    let content = fs::read_to_string(filename).unwrap();
-    let length = content.len();
-    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
-
-    stream.write_all(response.as_bytes()).unwrap();
+    app
 }
